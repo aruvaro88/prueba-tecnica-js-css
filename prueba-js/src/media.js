68ca8e156cdc7c92ea669be2f97f32d26a28d2d1
aruvaro88 <aruvaro@gmail.com>
2025-03-25 18:04:46 +0100

    average function

--- prueba-js/src/media.js
+++ prueba-js/src/media.js
@@ -1,5 +1,5 @@
 export default (json) => {
-    // Do your thing here!
-    return "";
-  };
-  
+  const totalAge = json.reduce((acum, elm) => acum + elm.edad, 0)
+  const avg = totalAge / json.length
+  return avg.toFixed(2)
+}
